21de62aa26a0ae8f8c6e8a1ff04e8a76d94ffeee
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-15 09:56:35 +0000

    crud

--- Backend/backend/src/services/projects/products/index.js
+++ Backend/backend/src/services/projects/products/index.js
@@ -13,4 +13,38 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+    const allProducts = await ProductModel.find();
+    res.send(allProducts);
+  } catch (error) {
+    console.log(error);
+    res.send("Somethings Gone Wrong");
+  }
+});
+
+router.get("/:productID", async (req, res) => {
+  try {
+    const selectedProduct = await ProductModel.findById(req.params.productID);
+    res.send(selectedProduct);
+  } catch (error) {
+    console.log(error);
+    res.send("Somethings Gone Wrong");
+  }
+});
+
+router.get("/:productID", async (req, res) => {
+  try {
+    const modifiedProduct = await ProductModel.findByIdAndUpdate(
+      req.params.productID,
+      req.body,
+      { runValidators: true, new: true }
+    );
+    res.send(modifiedProduct);
+  } catch (error) {
+    console.log(error);
+    res.send("Somethings Gone Wrong");
+  }
+});
+
 module.exports = router;
